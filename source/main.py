c1ae6335a9d6bc58449c4871d94693646402da7c
blah-blah <blah-blah@blah.(none)>
2014-07-22 22:25:36 +0300

    fixed bug with collision, moved Player to separate file

--- source/main.py
+++ source/main.py
@@ -1,6 +1,7 @@
 __author__ = 'matan'
 
 import pygame
+from Player import Player
 #import tmx
 
 
@@ -14,9 +15,6 @@ move_map = {pygame.K_LEFT: [-1, 0],
             pygame.K_SPACE: [0, -1],
             }
 
-def Clamp(value, min_value, max_value):
-    return min(max(value, min_value), max_value)
-
 class Box(pygame.sprite.Sprite):
     def __init__(self, size=(32, 32), pos=(0, 0)):
         pygame.sprite.Sprite.__init__(self)
@@ -27,77 +25,6 @@ class Box(pygame.sprite.Sprite):
 
     def update(self, dt):
         pass
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, *Group):
-        pygame.sprite.Sprite.__init__(self, Group)
-        self.image = pygame.Surface((32, 32))
-        self.size = (32, 32)
-        self.rect = pygame.Rect(0, 0, *self.size)
-        self.bound = None
-        self.speed = [0, 0]
-        self.max_speed_x = (-15, 15)
-        self.max_speed_y = (-100, 100)
-        self._onGround = False
-        self.slowdownAcceleration = 1
-        self.g = 1
-        self.image.fill(pygame.Color("#0000ff"))
-
-    def set_speed(self, speed):
-        self.set_speed_x(speed[0])
-        self.set_speed_y(speed[1])
-        self.__clamp_speed()
-        self._onGround = False
-
-    def set_speed_x(self, speedX):
-        self.speed[0] += speedX
-
-    def set_speed_y(self, speedY):
-        if self._onGround and speedY < 0 :
-            self.speed[1] = speedY
-
-    def update(self, dt):
-        self.__apply_acceleration()
-        self.__clamp_speed()
-        self.__collide()
-        self.rect.move_ip(*self.speed)
-        if self.bound:
-            self.rect.clamp_ip(self.bound)
-
-    def __clamp_speed(self):
-        self.speed[0] = max(min(self.speed[0], self.max_speed_x[1]), self.max_speed_x[0])
-        self.speed[1] = max(min(self.speed[1], self.max_speed_y[1]), self.max_speed_y[0])
-
-    def __apply_acceleration(self):
-        #G
-        if not self._onGround:
-            self.speed[1] += self.g
-        else:
-            self.speed[1] = 0
-
-        if self.speed[0] < 0:
-            self.speed[0] += self.slowdownAcceleration * 1
-        elif self.speed[0] > 0:
-            self.speed[0] += self.slowdownAcceleration * -1
-
-
-    def __collide(self):
-        temp_rect = self.rect.move(*self.speed)
-        for box in self.solid_objects.sprites():
-            if temp_rect.colliderect(box.rect):
-                if self.speed[1] > 0 and box.rect.top < temp_rect.bottom: #object is fallen
-                    self._onGround = True
-                    self.speed[1] = 0
-                if self.speed[1] < 0 and box.rect.bottom > temp_rect.top:
-                    self.speed[1] = 0
-
-        temp_rect = self.rect.move(*self.speed)
-        for box in self.solid_objects.sprites():
-           if temp_rect.colliderect(box.rect):
-                if self.speed[0] > 0 and temp_rect.right > box.rect.left:
-                    self.speed[0] = 0
-                if self.speed[0] < 0 and box.rect.right > temp_rect.left:
-                    self.speed[0] = 0
 
 
 class Game(object):
@@ -134,9 +61,9 @@ class Game(object):
             if pressed[pygame.K_SPACE]:
                 speed[1] = -500 * dt / 1000.0
             if pressed[pygame.K_LEFT]:
-                speed[0] = -100 * dt / 1000.0
+                speed[0] = -500 * dt / 1000.0
             if pressed[pygame.K_RIGHT]:
-                speed[0] = 100 * dt / 1000.0
+                speed[0] = 500 * dt / 1000.0
             player.set_speed(speed)
 
             screen.fill(pygame.Color("#000000"))
